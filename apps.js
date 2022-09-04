5cc527d14ff37b3af34ff92e7b0271ce366f15c2
Hammad Godil <hammadgodil@gmail.com>
2022-09-04 12:04:39 +0500

    sourcing my link

---
+++ apps.js
@@ -0,0 +1,28 @@
+document.addEventListener('keydown', function(e) {
+    if(e.keyCode === 37){
+        changeImage(-1);
+    } else if(e.keyCode === 39){
+        changeImage(1);
+    }
+}
+)
+
+function changeImage (direction) {
+    var imageContainer = document.getElementById("image");
+    var source = imageContainer.src;
+    var splittedSource = source.split("/");
+    var currentSrc = splittedSource[splittedSource.length - 1];
+    currentSrc = Number(currentSrc.split(".")[0]);
+    if(direction === 1) {
+        currentSrc++;
+    } else {
+        currentSrc--;
+    }
+    if (currentSrc > 7) {
+        currentSrc = 1;
+    } else if (currentSrc < 1) {
+        currentSrc = 7;
+    }
+    imageContainer.src = "images/" + currentSrc + ".jpg";
+    console.log(currentSrc)
+}
